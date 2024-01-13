c5becb0ab6ed44fc5da2e9f07199b157e3f78787
riczfe <s3750945@student.rmit.edu.au>
2024-01-13 22:56:12 +0700

    Update

---
+++ ADVANCED REQUIREMENTS SCRIPT/Kubernetes_Server/setup_kubernetes.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Install kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+# Install minikube
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+chmod +x minikube
+sudo cp minikube /usr/local/bin && rm minikube
+
+# Start minikube
+minikube start
+
+# Verify installation
+kubectl version
+minikube status
