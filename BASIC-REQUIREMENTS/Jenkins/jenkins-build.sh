cc74f05ded2dcbc58067c4983274ca978bceb2e7
Duy Nguyen <s3878076@rmit.edu.vn>
2024-01-17 04:05:05 +0700

    updated dockerfile and jenkins to add jenkisn user to group

--- BASIC-REQUIREMENTS/Jenkins/jenkins-build.sh
+++ BASIC-REQUIREMENTS/Jenkins/jenkins-build.sh
@@ -2,7 +2,7 @@ JENKINS_REPO_URI=959445117732.dkr.ecr.us-east-1.amazonaws.com/jenkins
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $JENKINS_REPO_URI
 
-docker build -t jenkins .
+docker build --build-arg DOCKER_GROUP_ID=$(getent group docker | cut -d: -f3) -t jenkins .
 
 docker tag jenkins:latest $JENKINS_REPO_URI:latest
 
