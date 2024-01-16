2b9ec7c88bc4c1ea6417667a1ef93ae0a1c83daa
Renuzit.V <nvminhduy2002@gmail.com>
2024-01-16 16:14:32 +0700

    added jenkins separated docker build and run scripts

--- BASIC-REQUIREMENTS/Database/db-script.sh
+++ BASIC-REQUIREMENTS/Database/db-script.sh
@@ -1,17 +1,8 @@
+# if we want to test the network connection on a docker container, uncomment the below lines
 DB_REPO_URI=959445117732.dkr.ecr.us-east-1.amazonaws.com/mariadb
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $DB_REPO_URI
+docker kill mariadb
 
-docker build -t mariadb .
+docker rm mariadb
 
-docker tag mariadb:latest $DB_REPO_URI:latest
-
-docker push $DB_REPO_URI:latest
-
-# if we want to test the network connection on a docker container, uncomment the below lines
-
-# docker kill mariadb
-
-# docker rm mariadb
-
-# docker run --name mariadb -p 3306:3306 -d --network test_network mariadb
+docker run --name mariadb -p 3306:3306 -d --network test_network $DB_REPO_URI:latest
--- BASIC-REQUIREMENTS/RMIT-Store/store-script.sh
+++ BASIC-REQUIREMENTS/RMIT-Store/store-script.sh
@@ -1,23 +1,9 @@
+# if we want to test the network connection on a docker container
+
 ECR_REPO_URI=959445117732.dkr.ecr.us-east-1.amazonaws.com/rmitstore
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REPO_URI
+docker kill rmitstore
 
-rm -rf COSC2767-RMIT-Store
+docker rm rmitstore
 
-git clone https://github.com/RenuzitV/COSC2767-RMIT-Store.git
-
-cd COSC2767-RMIT-Store
-
-docker build -t rmitstore .
-
-docker tag rmitstore:latest $ECR_REPO_URI:latest
-
-docker push $ECR_REPO_URI:latest
-
-# if we want to test the network connection on a docker container, uncomment the below lines
-
-# docker kill rmitstore
-
-# docker rm rmitstore
-
-# docker run -d -p 80:80 --name rmitstore --network test_network rmitstore:latest
+docker run -d -p 80:80 --name rmitstore --network test_network $ECR_REPO_URI:latest
