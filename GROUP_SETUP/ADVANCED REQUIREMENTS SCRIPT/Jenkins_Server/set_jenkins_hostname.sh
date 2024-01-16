fe654332e71fd0b542eaa62a5bbc4e35806f040f
riczfe <s3750945@student.rmit.edu.au>
2024-01-16 13:57:20 +0700

    update neccessary changes

--- GROUP_SETUP/ADVANCED REQUIREMENTS SCRIPT/Jenkins_Server/set_jenkins_hostname.sh
+++
@@ -1,8 +0,0 @@
-# Set the hostname
-echo "jenkins_server" > /etc/hostname
-
-# Inform the user to run the post-reboot script after the server restarts
-echo "Rebooting...."
-
-# Reboot the server
-reboot
--- test/uitests/README.md
+++
@@ -1,9 +0,0 @@
-How to run test:
-```
-cd .\test\uitests
-pip install pipenv
-pipenv install pytest
-pipenv install selenium
-pipenv install webdriver_manager
-pipenv run python -m pytest
-```
