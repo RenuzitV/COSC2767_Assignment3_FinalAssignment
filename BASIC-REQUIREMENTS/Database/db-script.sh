7c5b49990e171d7516469688f6eb634e804954f6
Duy Nguyen <s3878076@rmit.edu.vn>
2024-01-17 16:32:16 +0700

    cleanup

--- BASIC-REQUIREMENTS/Database/db-script.sh
+++
@@ -1,8 +0,0 @@
-# if we want to test the network connection on a docker container, uncomment the below lines
-DB_REPO_URI=959445117732.dkr.ecr.us-east-1.amazonaws.com/mariadb
-
-docker kill mariadb
-
-docker rm mariadb
-
-docker run --name mariadb -p 3306:3306 -d --network test_network $DB_REPO_URI:latest
